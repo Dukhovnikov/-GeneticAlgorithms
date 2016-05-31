8788d59a0081caced3a5ffc4753c18293fc36305
Danil Dukhovnikov <DukhovnikovDanil@yandex.ru>
2016-06-01 00:12:15 +0300

    Добавлен класс Популяция

--- GeneticAlgorithms/Individual.cs
+++ GeneticAlgorithms/Individual.cs
@@ -15,6 +15,66 @@ namespace GeneticAlgorithms
         /// Гены характеризующие генотип особи.
         /// </summary>
         private double[] gene { get; set; }
-         
+
+        /// <summary>
+        /// Исследуемая функция в виде лямбда выражения.
+        /// </summary>
+        private static Delegate Function { get; set; }
+
+        /// <summary>
+        /// Приспособленность особи / Значение функции в точке.
+        /// </summary>
+        private double FitnessFunction { get { return (double)Function.DynamicInvoke(gene); } }
+
+        /// <summary>
+        /// Значение отдельного гена.
+        /// </summary>
+        public double this[int index]
+        {
+            get { return gene[index]; } /// Аксессор для получения данных
+            set { gene[index] = value; } /// Аксессор для установки данных
+        }
+
+        /// <summary>
+        /// Сравнивание двух особей.
+        /// </summary>
+        public static bool operator ==(Individual o1, Individual o2)
+        {
+            int k = 0;
+            for (int i = 0; i < o1.Length; i++)
+            {
+                if (o1[i] == o2[i]) k++;
+            }
+            if (k == o1.Length) return true;
+            else return false;
+        }
+
+        /// <summary>
+        /// Сравнивание двух особей.
+        /// </summary>
+        public static bool operator !=(Individual o1, Individual o2)
+        {
+            int k = 0;
+            for (int i = 0; i < o1.Length; i++)
+            {
+                if (o1[i] == o2[i]) k++;
+            }
+            if (k == o1.Length) return false;
+            else return true;
+        }
+
+        /// <summary>
+        /// Размер особи / Количество хромосом.
+        /// </summary>
+        public int Length { get { return gene.Length; } }
+
+        /// <summary>
+        /// Нулевая особь, размерностью i.
+        /// </summary>
+        public Individual (int i)
+        {
+            gene = new double[i];
+            for (int j = 0; j < i; j++) { gene[i] = 0; }
+        }
     }
 }
---
+++ GeneticAlgorithms/Population.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithms
+{
+    /// <summary>
+    /// Класс реализующий популяцию, как набор особей.
+    /// </summary>
+    class Population
+    {
+        /// <summary>
+        /// Класс реализующий популяцию, как набор особей.
+        /// </summary>
+        private List<Individual> population { get; set; }
+
+        /// <summary>
+        /// Переменная для генерации случайного числа из заданного промежутка.
+        /// </summary>
+        static Random RandomNumber = new Random();
+
+        /// <summary>
+        /// Возвращает или задает значение отдельной особи.
+        /// </summary>
+        public Individual this[int index]
+        {
+            get { return population[index]; }
+            set { population[index] = value; }
+        }
+    }
+}
