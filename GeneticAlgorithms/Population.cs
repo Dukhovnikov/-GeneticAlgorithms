b4971e3cabd6a8c8e4fcee06c2a85b1ad6d7c844
Данил <reporter1995@mail.ru>
2016-06-01 01:17:30 +0300

    Класс особь заменен классом Вектор

--- GeneticAlgorithms/Population.cs
+++ GeneticAlgorithms/Population.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using myMatrix;
+using myVector;
 
 namespace GeneticAlgorithms
 {
@@ -14,7 +16,7 @@ namespace GeneticAlgorithms
         /// <summary>
         /// Класс реализующий популяцию, как набор особей.
         /// </summary>
-        private List<Individual> population { get; set; }
+        private List<Vector> population { get; set; }
 
         /// <summary>
         /// Переменная для генерации случайного числа из заданного промежутка.
@@ -22,12 +24,35 @@ namespace GeneticAlgorithms
         static Random RandomNumber = new Random();
 
         /// <summary>
+        /// Количество особей в популяции.
+        /// </summary>
+        private int Value { get; }
+
+        /// <summary>
         /// Возвращает или задает значение отдельной особи.
         /// </summary>
-        public Individual this[int index]
+        public Vector this[int index]
         {
             get { return population[index]; }
             set { population[index] = value; }
+        }
+
+        /// <summary>
+        /// Конструктор, генерирующий случайную популяцию заданного размера.
+        /// </summary>
+        public Population(Vector StartPoint, Vector EndPoint, int SizePopulation, int ChromosomeCount)
+        {
+            размер = РазмерПопуляции;
+            популяция = new Особь[РазмерПопуляции];
+            double delta = КонецПромежутка - НачалоПромежутка;
+            for (int i = 0; i < РазмерПопуляции; i++)
+            {
+                популяция[i] = new Особь(КоличествоХромосом);
+                for (int j = 0; j < КоличествоХромосом; j++)
+                {
+                    популяция[i][j] = RandomNumber.NextDouble() * (КонецПромежутка - НачалоПромежутка) + НачалоПромежутка;
+                }
+            }
         }
     }
 }
