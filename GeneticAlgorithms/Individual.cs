efc65894cac56002a5acdd3737cf4b3c4733241d
Danil Dukhovnikov <DukhovnikovDanil@yandex.ru>
2016-05-31 23:38:17 +0300

    Создан класс Особи

---
+++ GeneticAlgorithms/Individual.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithms
+{
+    /// <summary>
+    /// Исскуственный класс представляющий собой отдельную особь.
+    /// </summary>
+    class Individual
+    {
+        /// <summary>
+        /// Гены характеризующие генотип особи.
+        /// </summary>
+        private double[] gene { get; set; }
+         
+    }
+}
